873fb631bafaa567ba4c7fb8b5faf88383d67e1f
AlexandreFoucherDEV <foucher.alex@gmail.com>
2023-02-28 14:17:52 +0100

    post/update experience/soft_skill

--- service/soft_skillService.js
+++ service/soft_skillService.js
@@ -3,15 +3,14 @@ const UserProfile = require('../model/userProfileModel');
 const { v4: uuidv4 } = require('uuid');
 
 const postOneSoft_skill = async (data) => {
-    const {soft_skill} = data;
-    const {uuid_user} = data;
+    const {soft_skill, user} = data;
 
-    const userData = await User.findOne({uuid : uuid_user}).populate('profile');
+    const userData = await User.findOne({uuid : user.userId}).populate('profile');
     if(userData) {
         const userProfile = userData.profile;
         if(userProfile){
-            soft_skill[0].uuid = uuidv4();
-            userProfile.soft_skill.push(soft_skill[0]);
+            soft_skill.uuid = uuidv4();
+            userProfile.soft_skill.push(soft_skill);
             await userProfile.save(function (err,doc){
                 if(err){
                     return err
@@ -28,15 +27,15 @@ const postOneSoft_skill = async (data) => {
 }
 
 const updateOneSoft_skill = async (data) => {
-    const {soft_skill,uuid_user} = data;
-    const userData = await User.findOne({uuid : uuid_user}).populate('profile');
+    const {soft_skill, user} = data;
+    const userData = await User.findOne({uuid : user.userId}).populate('profile');
     if( userData){
         const userProfile = userData.profile;
         if(userProfile){
             console.log(soft_skill)
-            const found = userProfile.soft_skill.findIndex(e => e.uuid === soft_skill[0].uuid);
+            const found = userProfile.soft_skill.findIndex(e => e.uuid === soft_skill.uuid);
             if(found >=0){
-                userProfile.soft_skill[found] = soft_skill[0];
+                userProfile.soft_skill[found] = soft_skill;
                 await userProfile.save(function (err,doc){
                     if(err){
                         return err
